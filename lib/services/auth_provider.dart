ae7e1ffcaaa021c1bd054451aa59ae30428ca9db
micky-smart-001 <owolabim950@gmail.com>
2026-07-02 19:20:36 +0100

    Fix: Correct import path in auth_provider.dart

--- lib/services/auth_provider.dart
+++ lib/services/auth_provider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../services/auth_service.dart';
+import 'auth_service.dart';
 
 class AuthProvider extends ChangeNotifier {
   final AuthService _service =
